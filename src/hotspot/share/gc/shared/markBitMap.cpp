1cf7fe6c2effb7792818aa9b325ae9188602f8c0
Aleksey Shipilev <shade@openjdk.org>
2018-11-15 17:03:17 +0100

    Cherry-pick: 8213373: Bulk MarkBitMap clearing methods

--- src/hotspot/share/gc/shared/markBitMap.cpp
+++ src/hotspot/share/gc/shared/markBitMap.cpp
@@ -44,18 +44,19 @@ void MarkBitMap::initialize(MemRegion heap, MemRegion storage) {
   _bm = BitMapView((BitMap::bm_word_t*) storage.start(), _covered.word_size() >> _shifter);
 }
 
-void MarkBitMap::clear() {
-  clear_range_large(_covered);
-}
-
-void MarkBitMap::clear_range(MemRegion mr) {
+void MarkBitMap::do_clear(MemRegion mr, bool large) {
   MemRegion intersection = mr.intersection(_covered);
   assert(!intersection.is_empty(),
          "Given range from " PTR_FORMAT " to " PTR_FORMAT " is completely outside the heap",
          p2i(mr.start()), p2i(mr.end()));
   // convert address range into offset range
-  _bm.at_put_range(addr_to_offset(intersection.start()),
-                   addr_to_offset(intersection.end()), false);
+  size_t beg = addr_to_offset(intersection.start());
+  size_t end = addr_to_offset(intersection.end());
+  if (large) {
+    _bm.clear_large_range(beg, end);
+  } else {
+    _bm.clear_range(beg, end);
+  }
 }
 
 #ifdef ASSERT
@@ -65,11 +66,3 @@ void MarkBitMap::check_mark(HeapWord* addr) {
          p2i(this), p2i(addr));
 }
 #endif
-
-void MarkBitMap::clear_range_large(MemRegion mr) {
-  MemRegion intersection = mr.intersection(_covered);
-  assert(!intersection.is_empty(), "unexpected empty region");
-  // convert address range into offset range
-  _bm.clear_large_range(addr_to_offset(intersection.start()),
-                        addr_to_offset(intersection.end()));
-}
