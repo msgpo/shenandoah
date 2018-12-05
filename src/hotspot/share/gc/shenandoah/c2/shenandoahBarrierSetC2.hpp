21d14c02a7d9e61aaec16caf57a23780f97158a1
Roman Kennke <rkennke@openjdk.org>
2018-12-05 17:57:43 +0100

    Clean up leftover acmp RB in ShenandoahBarrierC2

--- src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.hpp
+++ src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.hpp
@@ -99,8 +99,6 @@ public:
 
   ShenandoahBarrierSetC2State* state() const;
 
-  Node* shenandoah_read_barrier_acmp(GraphKit* kit, Node* obj);
-
   static const TypeFunc* write_ref_field_pre_entry_Type();
   static const TypeFunc* shenandoah_clone_barrier_Type();
   static const TypeFunc* shenandoah_write_barrier_Type();
